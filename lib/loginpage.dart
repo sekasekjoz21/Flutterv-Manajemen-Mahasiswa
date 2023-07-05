7203d6bf4f0c42da3a64f8e73e5faef5f999b7ff
sekasekjoz21 <rachmanuryani21@gmail.com>
2023-07-05 17:13:49 +0700

    first commit

---
+++ lib/loginpage.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<String> loginUser(String email, String password) async {
+  final response = await http.post(
+    Uri.parse('http://192.168.221.70/flutterapi/crudflutter/login.php'),
+    body: {
+      'email': email,
+      'password': password,
+    },
+  );
+  print(response.body);
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    if (data == 'Success') {
+      return 'Success';
+    } else {
+      return 'Error';
+    }
+  } else {
+    throw Exception('Failed to connect to server');
+  }
+}
